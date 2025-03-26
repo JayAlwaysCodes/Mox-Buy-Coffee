3cff7d61c9c83554707d92d1dcc44b648c86fd98
jayalwayscodes <johnsonjardocs@gmail.com>
2025-03-26 18:01:58 +0100

    wrote more test

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,4 +1,8 @@
 from eth_utils import to_wei
+import boa
+
+SEND_VALUE = to_wei(1, "ether")
+RANDOM_USER = boa.env.generate_address("non-owner")
 
 def test_price_feed_is_correct(coffee, eth_usd):
     assert coffee.PRICE_FEED() == eth_usd.address
@@ -6,3 +10,27 @@ def test_price_feed_is_correct(coffee, eth_usd):
 def test_starting_values(coffee, account):
     assert coffee.MINIMUM_USD() == to_wei(5, "ether")
     assert coffee.OWNER() == account.address
+
+def test_fund_fails_without_enough_eth(coffee):
+    with boa.reverts():
+        coffee.fund()
+
+def test_fund_with_money(coffee, account):
+    #arrange
+    boa.env.set_balance(account.address, SEND_VALUE)
+    #act
+    coffee.fund(value=SEND_VALUE)
+    #assert
+    funder = coffee.funders(0)
+    assert funder == account.address
+    assert coffee.funder_to_amount_funded(funder) == SEND_VALUE
+
+
+def test_non_owner_cannot_withdraw(coffee, account):
+    #arrange
+    boa.env.set_balance(account.address, SEND_VALUE)
+    coffee.fund(value=SEND_VALUE)
+
+    with boa.env.prank(RANDOM_USER):
+        with boa.reverts():
+            coffee.withdraw()
