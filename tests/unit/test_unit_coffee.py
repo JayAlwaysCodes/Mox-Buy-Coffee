eb8041fd458517d01a077749f0d37a6eea3fec74
jayalwayscodes <johnsonjardocs@gmail.com>
2025-03-27 09:28:08 +0100

    refactored my tests

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,7 +1,8 @@
 from eth_utils import to_wei
 import boa
+from tests.conftest import SEND_VALUE
 
-SEND_VALUE = to_wei(1, "ether")
+
 RANDOM_USER = boa.env.generate_address("non-owner")
 NUM_FUNDERS = 10
 
@@ -16,33 +17,28 @@ def test_fund_fails_without_enough_eth(coffee):
     with boa.reverts():
         coffee.fund()
 
-def test_fund_with_money(coffee, account):
-    #arrange
-    boa.env.set_balance(account.address, SEND_VALUE)
-    #act
-    coffee.fund(value=SEND_VALUE)
+def test_fund_with_money(coffee_funded, account):
+    
     #assert
-    funder = coffee.funders(0)
+    funder = coffee_funded.funders(0)
     assert funder == account.address
-    assert coffee.funder_to_amount_funded(funder) == SEND_VALUE
+    assert coffee_funded.funder_to_amount_funded(funder) == SEND_VALUE
 
 
-def test_non_owner_cannot_withdraw(coffee, account):
-    #arrange
-    boa.env.set_balance(account.address, SEND_VALUE)
-    coffee.fund(value=SEND_VALUE)
+def test_non_owner_cannot_withdraw(coffee_funded):
+   
+    
 
     with boa.env.prank(RANDOM_USER):
         with boa.reverts():
-            coffee.withdraw()
+            coffee_funded.withdraw()
 
 
-def test_owner_can_withdraw(coffee):
-    boa.env.set_balance(coffee.OWNER(), SEND_VALUE)
-    with boa.env.prank(coffee.OWNER()):
-        coffee.fund(value=SEND_VALUE)
-        coffee.withdraw()
-    assert boa.env.get_balance(coffee.address) == 0
+def test_owner_can_withdraw(coffee_funded):
+    with boa.env.prank(coffee_funded.OWNER()):
+        
+        coffee_funded.withdraw()
+    assert boa.env.get_balance(coffee_funded.address) == 0
 
 
 def test_multiple_funders_and_withdraw(coffee, account):
